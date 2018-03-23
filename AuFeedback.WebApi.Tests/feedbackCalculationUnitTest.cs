82506331847cb2eb7c6763fee754b2751115fdec
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-03-23 17:02:30 +1100

    Add more unit tests.

--- AuFeedback.WebApi.Tests/feedbackCalculationUnitTest.cs
+++ AuFeedback.WebApi.Tests/feedbackCalculationUnitTest.cs
@@ -12,6 +12,64 @@ namespace AuFeedback.WebApi.Tests
     public class feedbackCalculationUnitTest
     {
         [TestMethod]
+        public void ShouldCalculateDaysLessThan180()
+        {
+
+            var userRecords = new List<UserRecord>
+            {
+                new UserRecord {UserName = "TestUser", FeedbackDate=DateTime.Now.AddDays(-10).ToString("yyyyMMdd"), QuestionType="green" }
+            };
+
+            var mockRepository = Substitute.For<IRepository>();
+            mockRepository.ReadFiles(Arg.Any<string>()).Returns(userRecords);
+
+            var feedbackCalculation = new FeedbackCalculation(string.Empty, mockRepository);
+            var timeUp = feedbackCalculation.GetTimeUp("TestUser", "green");
+
+            Assert.IsTrue(timeUp.IsRecordExisting);
+            Assert.IsFalse(timeUp.IsTimeUp ?? true);
+        }
+
+        [TestMethod]
+        public void ShouldCalculateDaysEqual180()
+        {
+
+            var userRecords = new List<UserRecord>
+            {
+                new UserRecord {UserName = "TestUser", FeedbackDate=DateTime.Now.AddDays(-180).ToString("yyyyMMdd"), QuestionType="red" }
+            };
+
+            var mockRepository = Substitute.For<IRepository>();
+            mockRepository.ReadFiles(Arg.Any<string>()).Returns(userRecords);
+
+            var feedbackCalculation = new FeedbackCalculation(string.Empty, mockRepository);
+            var timeUp = feedbackCalculation.GetTimeUp("TestUser", "red");
+
+            Assert.IsTrue(timeUp.IsRecordExisting);
+            Assert.IsFalse(timeUp.IsTimeUp ?? true);
+        }
+
+        [TestMethod]
+        public void ShouldCalculateDaysMoreThan180()
+        {
+
+            var userRecords = new List<UserRecord>
+            {
+                new UserRecord {UserName = "TestUser", FeedbackDate=DateTime.Now.AddDays(-200).ToString("yyyyMMdd"), QuestionType="site" }
+            };
+
+            var mockRepository = Substitute.For<IRepository>();
+            mockRepository.ReadFiles(Arg.Any<string>()).Returns(userRecords);
+
+            var feedbackCalculation = new FeedbackCalculation(string.Empty, mockRepository);
+            var timeUp = feedbackCalculation.GetTimeUp("TestUser", "site");
+
+            Assert.IsTrue(timeUp.IsRecordExisting);
+            Assert.IsTrue(timeUp.IsTimeUp ?? false);
+        }
+
+
+        [TestMethod]
         public void ShouldSaveUser()
         {
             var userRecords = new List<UserRecord>
@@ -32,25 +90,5 @@ namespace AuFeedback.WebApi.Tests
 
         }
 
-        [TestMethod]
-        public void ShouldCalculateDays()
-        {
-
-            var userRecords = new List<UserRecord>
-            {
-                new UserRecord {UserName = "TestUser", FeedbackDate=DateTime.Now.ToString("yyyyMMdd"), QuestionType="green" }
-            };
-
-            var mockRepository = Substitute.For<IRepository>();
-            mockRepository.ReadFiles(Arg.Any<string>()).Returns(userRecords);
-            mockRepository.WriteRecord(Arg.Any<string>(), userRecords);
-
-            var feedbackCalculation = new FeedbackCalculation(string.Empty, mockRepository);
-            feedbackCalculation.AddUpdateFeedbackDate("TestUser", "green");
-            var timeUp = feedbackCalculation.GetTimeUp("TestUser", "green");
-
-            Assert.IsTrue(timeUp.IsRecordExisting);
-            Assert.IsFalse(timeUp.IsTimeUp ?? true);
-        }
     }
 }
